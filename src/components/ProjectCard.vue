24ed75557b8be998dbfeb07b7dbf6bb8472a2eb8
LeonardoMayo <jan_leppich@web.de>
2021-03-06 13:57:16 +0100

    js logic of projects list

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-card elevation="4" outlined color="white" style="width: 75%; align-self: center" class="ma-auto">
+        <v-card-text class="pa-0">
+            <v-row class="ma-0">
+                <v-col cols="6" class="pa-0">
+                    <div class="projectImage"
+                         :style="`background-image: url(${ titleImg });`"/>
+                </v-col>
+                <v-col cols="6"  style="color: black">
+                    <v-card-title class="pb-0">{{ title }}</v-card-title>
+                    <v-divider></v-divider>
+                    <v-card-text>
+                        {{ smallDescription }}
+                    </v-card-text>
+                    <v-card-actions class="pa-0">
+                        <v-spacer></v-spacer>
+                        <v-btn color="secondary" text style="align-self: end"
+                               @click="routeToArticle">
+                            Mehr >
+                        </v-btn>
+                    </v-card-actions>
+                </v-col>
+            </v-row>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        title: { required: true, type: String },
+        smallDescription: { required: true, type: String },
+        titleImg: { required: true, type: String },
+        linkToArticle: { required: true, type: String }
+    },
+    methods: {
+        routeToArticle() {
+            this.$router.push(this.linkToArticle);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.projectImage {
+    height: 250px !important;
+    width: 100%;
+    background-size: cover;
+    overflow: hidden;
+}
+</style>
--- src/components/ProjectsList.vue
+++ src/components/ProjectsList.vue
@@ -7,30 +7,14 @@
                         show-arrows-on-hover>
                 <v-carousel-item>
                     <div style="height: 100%; display: flex">
-                        <v-card elevation="4" outlined color="white" style="width: 75%; align-self: center" class="ma-auto">
-                            <v-card-text class="pa-0">
-                                <v-row class="ma-0">
-                                    <v-col cols="6" class="pa-0">
-                                        <div class="projectImage"/>
-                                    </v-col>
-                                    <v-col cols="6"  style="color: black">
-                                        <v-card-title class="pb-0">AR PvPlattform</v-card-title>
-                                        <v-divider></v-divider>
-                                        <v-card-text>
-                                            Ein Projekt für ein 1v1 Augmented Reality Game in dem Spieler sich
-                                            ihren Weg zum Ziel bauen müssen, in einer AR Umgebung.
-                                            Dieses Projekt war Teil meiner Bachelor-Thesis.
-                                        </v-card-text>
-                                        <v-card-actions class="pa-0">
-                                            <v-spacer></v-spacer>
-                                            <v-btn color="secondary" text style="align-self: end">
-                                                Mehr >
-                                            </v-btn>
-                                        </v-card-actions>
-                                    </v-col>
-                                </v-row>
-                            </v-card-text>
-                        </v-card>
+                        <ProjectCard
+                            :title="'AR PvPlattform'"
+                            :title-img="'https://cdn.vuetifyjs.com/images/parallax/material.jpg'"
+                            :small-description="'Ein Projekt für ein 1v1 Augmented Reality Game in dem ' +
+                             'Spieler sich ihren Weg zum Ziel bauen müssen, in einer AR Umgebung. ' +
+                             'Dieses Projekt war Teil meiner Bachelor-Thesis.'"
+                            :link-to-article="'/ar-pvplatform/datenschutz'"
+                        />
                     </div>
                 </v-carousel-item>
             </v-carousel>
@@ -39,8 +23,10 @@
 </template>
 
 <script>
+import ProjectCard from "@/components/ProjectCard";
 export default {
     name: 'ProjectsList',
+    components: {ProjectCard},
     data: () => ({
         model: true,
     }),
@@ -48,11 +34,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.projectImage {
-    background-image: url("https://cdn.vuetifyjs.com/images/parallax/material.jpg");
-    height: 250px !important;
-    width: 100%;
-    background-size: cover;
-    overflow: hidden;
-}
+
 </style>
