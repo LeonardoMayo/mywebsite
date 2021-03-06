a37a0e50793f4c3f3580577444c0b823223ed341
LeonardoMayo <jan_leppich@web.de>
2021-03-06 13:32:57 +0100

    worked on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-    <v-card flat tile class="grey lighten-3 text-center">
+    <v-card flat tile class="grey lighten-3 text-center" width="100%">
         <v-card-text>
             <v-tooltip bottom v-for="icon in icons" :key="icon">
                 <template v-slot:activator="{ on, attrs }">
-                    <v-btn class="mx-4 " icon v-bind="attrs" v-on="on">
+                    <v-btn class="mx-4" icon v-bind="attrs" v-on="on" @click="openLink(icon.link)">
                         <v-icon size="24px">
                             {{ icon.icon }}
                         </v-icon>
@@ -14,8 +14,11 @@
 
         </v-card-text>
 
-        <v-card-text class=" pt-0">
-            Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        <v-divider></v-divider>
+
+        <v-card-text>
+                <a class="footerLinks">AR PvPlatform Datenschutzbestimmung</a>
+                <a class="footerLinks">Impressum</a>
         </v-card-text>
 
         <v-divider></v-divider>
@@ -31,19 +34,29 @@ export default {
     name: 'Footer',
     data: () => ({
         icons: [
-            { name: 'Youtube', icon: 'mdi-youtube' },
-            { name: 'Twitter', icon: 'mdi-twitter' },
-            { name: 'Instagram', icon: 'mdi-instagram' },
-            { name: 'Thingiverse', icon: 'mdi-printer-3d' },
-            { name: 'Github', icon: 'mdi-github' },
-            { name: 'StackOverflow', icon: 'mdi-stack-overflow' },
-            { name: 'Xing', icon: 'mdi-xing' },
-            { name: 'LinkedIn', icon: 'mdi-linkedin' },
+            { name: 'Youtube', icon: 'mdi-youtube', link: 'https://www.youtube.com/channel/UCPscWtfnpPcChkWBu1u-vZQ' },
+            { name: 'Twitter', icon: 'mdi-twitter', link: 'https://twitter.com/AvocadoUkulele' },
+            { name: 'Instagram', icon: 'mdi-instagram', link: 'https://www.instagram.com/jpl_fotography/' },
+            { name: 'Thingiverse', icon: 'mdi-printer-3d', link: 'https://www.thingiverse.com/' },
+            { name: 'Github', icon: 'mdi-github', link: 'https://github.com/LeonardoMayo' },
+            { name: 'StackOverflow', icon: 'mdi-stack-overflow', link: 'https://stackoverflow.com/' },
+            { name: 'Xing', icon: 'mdi-xing', link: 'https://www.xing.com/profile/Jan_Leppich/cv' },
+            { name: 'LinkedIn', icon: 'mdi-linkedin', link: 'https://de.linkedin.com/' },
         ],
     }),
+    methods: {
+        openLink(link) {
+            window.open(link)
+        }
+    }
 }
 </script>
 
 <style scoped lang="scss">
-
+.footerLinks {
+    color: grey !important;
+    margin: 0.25em;
+    text-underline: grey;
+    text-decoration: underline;
+}
 </style>
