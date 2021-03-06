4fdef08d6f1de3cb76010749ee93b197eaddff16
LeonardoMayo <jan_leppich@web.de>
2021-03-06 13:56:50 +0100

    Logo added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,26 +1,28 @@
 <template>
     <v-parallax class="pa-0" dark height="1000"
-        src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+        src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
     >
         <v-row align="center" justify="center" class="pa-0">
             <v-col class="text-center pa-0" cols="12">
-                <h1 class="display-1 font-weight-thin mb-4">
-                    JPL Photo and Software
-                </h1>
-                <h4 class="subheading">
-                    www.jpl-photo-and.software
-                </h4>
+                <img :src="logoBunt" class="display-1" id="logo"/>
             </v-col>
         </v-row>
     </v-parallax>
 </template>
 
 <script>
+import logoBunt from '@/assets/Bilder/jpl-f02-01.png'
+
 export default {
-    name: 'Header'
+    name: 'Header',
+    data: () => ({
+       logoBunt
+    }),
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+#logo {
+    width: 35%;
+}
 </style>
